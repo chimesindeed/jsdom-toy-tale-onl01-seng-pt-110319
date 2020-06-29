e939223fc3bd800fa65b73cc5fb97e0fac93114b
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-29 04:19:55 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  const toyCollection = document.querySelector("#toy-collection")
+  fetch("")
 });
